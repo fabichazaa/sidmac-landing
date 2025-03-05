93eb9f7d3510f3e973e59cb7bcd6aa39b3b3af47
Fabiola <fabiola.a.chazarreta@gmail.com>
2025-03-04 21:18:46 -0300

    Add header

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleHeaderLinks() {
+    let headerLinks = document.getElementById("header-links");
+    if (headerLinks.style.display === "block") {
+        headerLinks.style.display = "none";
+    } else {
+        headerLinks.style.display = "block";
+    }
+}
